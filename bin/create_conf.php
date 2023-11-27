36a3db9fc8147e798573a0f69a0d83f0120ee8db
mschlenstedt <Michael@schlenn.net>
2023-11-27 19:40:38 +0100

    Add more meta data

--- bin/create_conf.php
+++ bin/create_conf.php
@@ -103,6 +103,8 @@ $file = fopen("$lbpconfigdir/$rtl_433_configfile","w",1);
 	#fwrite($file,"analyze_pulses false\r\n");
 	fwrite($file,"report_meta time:unix\r\n");
 	fwrite($file,"report_meta protocol\r\n");
+	fwrite($file,"report_meta noise\r\n");
+	fwrite($file,"report_meta level\r\n");
 	#fwrite($file,"signal_grabber none\r\n");
 	# MQTT Credentials and output
 	fwrite($file,"output mqtt://".$creds['brokeraddress'].",user=".$creds['brokeruser'].",pass=".$creds['brokerpass'].",retain=0,devices=rtl_433[/protocol][/model][/id]\r\n");
